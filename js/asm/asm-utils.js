e425fd28a3542fab1dd19822d9ba8d5e2b40fe3c
HoverCatz <1442391+HoverCatz@users.noreply.github.com>
2022-11-18 18:04:06 +0100

    Update asm-utils.js

--- js/asm/asm-utils.js
+++ js/asm/asm-utils.js
@@ -58,6 +58,72 @@ function readUInt(arr, offset) {
     return r >>> 0;
 }
 
+function readClass(constantUtf8Values, offset, cpInfoOffsets, arr) {
+    return readStringish(constantUtf8Values, offset, cpInfoOffsets, arr);
+}
+function readStringish(constantUtf8Values, offset, cpInfoOffsets, arr) {
+    return readUTF8(constantUtf8Values, cpInfoOffsets[readUnsignedShort(arr, offset)], cpInfoOffsets, arr);
+}
+function readUTF8(constantUtf8Values, offset, cpInfoOffsets, arr) {
+    const cpPoolEntryIndex = readUnsignedShort(arr, offset);
+    if (offset === 0 || cpPoolEntryIndex === 0)
+        return null;
+    return readUtf(constantUtf8Values, cpPoolEntryIndex, cpInfoOffsets, arr);
+}
+function readUtf(constantUtf8Values, cpPoolEntryIndex, cpInfoOffsets, arr) {
+    const value = constantUtf8Values[cpPoolEntryIndex];
+    if (value !== null)
+        return value;
+    const cpInfoOffset = cpInfoOffsets[cpPoolEntryIndex];
+    return constantUtf8Values[cpPoolEntryIndex] = readUtf2(cpInfoOffset + 2, readUnsignedShort(arr, cpInfoOffset), arr);
+}
+function readUtf2(utfOffset, utfLength, arr) {
+    let currentOffset = utfOffset;
+    let endOffset = currentOffset + utfLength;
+    let result = '';
+    for (; currentOffset < endOffset; ) {
+        const currentByte = readByte(arr, currentOffset++);
+        if ((currentByte & 0x80) === 0)
+            result += String.fromCharCode(currentByte & 0x7F);
+        else
+        if ((currentByte & 0xE0) === 0xC0)
+            result += String.fromCharCode(((currentByte & 0x1F) << 6) + (readByte(arr, currentOffset++) & 0x3F));
+        else
+            result += String.fromCharCode(((currentByte & 0xF) << 12) + ((readByte(arr, currentOffset++) & 0x3F) << 6) + (readByte(arr, currentOffset++) & 0x3F));
+    }
+    return result;
+}
+
+function getTypeDescriptor(desc) {
+    if (desc.startsWith('['))
+        return desc;
+    return 'L' + desc + ';';
+}
+
+function getMethodDescriptor(desc) {
+    if (desc.startsWith('['))
+        return desc;
+    return 'L' + desc + ';';
+}
+
+function readConst(constantUtf8Values, cpInfoOffsets, constantPoolEntryIndex, arr) {
+    let cpInfoOffset = cpInfoOffsets[constantPoolEntryIndex];
+    switch (arr[cpInfoOffset - 1]) {
+        case 3:
+        case 4:
+        case 5:
+        case 6: return readUInt(arr, cpInfoOffset);
+        case 7: return getTypeDescriptor(readUTF8(constantUtf8Values, cpInfoOffset, cpInfoOffsets));
+        case 8: return readUTF8(constantUtf8Values, cpInfoOffset, cpInfoOffsets);
+
+        /* Java8 stuff? */
+        case 15:
+        case 16:
+        case 17: throw new Error("Not supported.");
+    }
+    return '';
+}
+
 /* ########## */
 
 // Write-functions
