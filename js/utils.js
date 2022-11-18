59bbe2cd3ce8d3ec3f2c4aed32b12f150921ec7e
HoverCatz <1442391+HoverCatz@users.noreply.github.com>
2022-11-18 17:28:15 +0100

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,19 +1,46 @@
 /* Attempt deep-cloning objects */
-const cloneObject = (input) => {
-    if (input instanceof String || input instanceof Number) {
+function deepCloneObject(input, cache = null) {
+    if (input == null)
         return input;
-    }
-    if (typeof input === 'object') {
-        const cloned = Object.assign({}, input); // Attempt deep-cloning
-        if (compareObjects(input, cloned))
+    const type = typeof input;
+    if (type === 'string' || type === 'number' ||
+        type === 'boolean' || type === 'function' ||
+        type === 'bigint')
+        return input;
+    if (type === 'object') {
+        if (input instanceof Date)
+            return new Date(input.getTime());
+        if (cache !== null && cache.has(input))
+            return cache.get(input);
+        if (input instanceof Array) {
+            const len = input.length;
+            const cloned = [...Array(len)];
+            if (cache === null) cache = new WeakMap();
+            cache.set(input, cloned);
+            for (let i = 0; i < len; i++)
+                cloned[i] = deepCloneObject(input[i], cache);
             return cloned;
+        }
+        if (input instanceof Object) {
+            const cloned = {};
+            if (cache === null) cache = new WeakMap();
+            cache.set(input, cloned);
+            for (let attr in input)
+                if (input.hasOwnProperty(attr))
+                    cloned[attr] = deepCloneObject(input[attr], cache);
+            return cloned;
+        }
     }
+    console.warn('Unknown type `' + type + '`, not sure if deepCloneObject works or not.');
     return input;
 }
 
 /* Compare two objects */
-const compareObjects = (in1, in2) => {
-    if (in1 === in2) return true;
+function compareObjects(in1, in2, strict = false) {
+    if ((!strict && in1 == in2) || (strict && in1 === in2 ))
+        return true;
+    if (in1 == null && in2 != null) return false;
+    if (in2 == null && in1 != null) return false;
     try {
         const json1 = JSON.stringify(in1);
         const json2 = JSON.stringify(in2);
@@ -27,4 +54,40 @@ const compareObjects = (in1, in2) => {
             return true;
     } catch { }
     return false;
+}
+
+/* Case object from A to B */
+function castObjectTo(obj, toType) {
+    if (obj == null) return obj;
+    const type = typeof obj;
+    switch (toType) {
+        case 'int':
+        case 'number': {
+            if (type === 'string')
+                return parseInt(obj);
+            if (type === 'number')
+                return obj;
+            if (type === 'boolean')
+                return (obj === true) ? 1 : 0;
+            return parseInt(obj.toString());
+        } break;
+        case 'string': {
+            if (type === 'string')
+                return obj;
+            if (type === 'boolean')
+                return (obj === true) ? 'true' : 'false';
+            return obj.toString();
+        } break;
+        case 'boolean': {
+            if (type === 'string')
+                return obj.toString().toLowerCase() === 'true';
+            if (type === 'number')
+                return obj.toString() !== '0';
+            if (type === 'boolean')
+                return obj;
+            // Warning
+        } break;
+    }
+    console.warn('Not sure what to cast here? Type=' + type + ', ToType=' + toType);
+    return obj;
 }
