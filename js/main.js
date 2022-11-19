82661eef9b4ce12be1fe7f381a109709f322e184
HoverCatz <1442391+HoverCatz@users.noreply.github.com>
2022-11-19 17:39:15 +0100

    Testing

--- js/main.js
+++ js/main.js
@@ -1,27 +1,43 @@
 // New attempt
 (async () => {
 
-    let buffer = [];
-    let currentIndex = 0;
+    const clz = new JvmClass('n/e/k/o', Opcodes.ACC_PUBLIC, 'TestClass');
 
-    currentIndex += writeUInt(buffer, 0xCAFEBABE);
-    console.log(currentIndex);
-    currentIndex += writeUInt(buffer, 0xCAFEBABE);
-    console.log(currentIndex);
-    currentIndex += writeUInt(buffer, 0xCAFEBABE);
-    console.log(currentIndex);
+    const testField = new JvmField(clz, Opcodes.ACC_PUBLIC, 'test', 'I');
+    const testFieldStatic = new JvmField(clz, Opcodes.ACC_STATIC, 'test_static', 'I');
+    const fields = {};
+    addToList(fields, testFieldStatic.fieldName + '.' + testFieldStatic.fieldDesc, testFieldStatic.getOrCreate());
+    addToList(fields, testField.fieldName + '.' + testField.fieldDesc, testField.getOrCreate());
 
-    currentIndex = 0;
-    let magic = readUInt(buffer, currentIndex);
-    console.log(magic.toString(16));
-    currentIndex += 4;
+    const testFunction = new JvmFunction(clz, Opcodes.ACC_PUBLIC, '', '');
+    const functions = {};
+    addToList(functions, 'test()V', testFunction);
+    clz.load(fields, functions);
 
-    magic = readUInt(buffer, currentIndex);
-    console.log(magic.toString(16));
-    currentIndex += 4;
+    const foundField = clz.findField('test()V', false);
+    console.log('found:', foundField);
 
-    magic = readUInt(buffer, currentIndex);
-    console.log(magic.toString(16));
-    currentIndex += 4;
+    // let buffer = [];
+    // let currentIndex = 0;
+    //
+    // currentIndex += writeUInt(buffer, 0xCAFEBABE);
+    // console.log(currentIndex);
+    // currentIndex += writeUInt(buffer, 0xCAFEBABE);
+    // console.log(currentIndex);
+    // currentIndex += writeUInt(buffer, 0xCAFEBABE);
+    // console.log(currentIndex);
+    //
+    // currentIndex = 0;
+    // let magic = readUInt(buffer, currentIndex);
+    // console.log(magic.toString(16));
+    // currentIndex += 4;
+    //
+    // magic = readUInt(buffer, currentIndex);
+    // console.log(magic.toString(16));
+    // currentIndex += 4;
+    //
+    // magic = readUInt(buffer, currentIndex);
+    // console.log(magic.toString(16));
+    // currentIndex += 4;
 
 })();
