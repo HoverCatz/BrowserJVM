9949fe7d1b3019eeed5a7044c79ea2f37bdd1a73
HoverCatz <1442391+HoverCatz@users.noreply.github.com>
2022-11-30 18:45:05 +0100

    Create JvmError.js

---
+++ js/shared/asm/objects/JvmError.js
@@ -0,0 +1,10 @@
+class JvmError extends Error {
+
+    type; // type string (java/lang/Exception, java/lang/ArithmeticException, etc)
+
+    constructor(message, type) {
+        super(message);
+        this.type = type;
+    }
+
+}
