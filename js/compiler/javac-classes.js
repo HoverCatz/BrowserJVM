14de2845a836639474e263bb7ab40698bf670f86
HoverCatz <1442391+HoverCatz@users.noreply.github.com>
2022-12-06 08:05:28 +0100

    Update javac-classes.js

--- js/compiler/javac-classes.js
+++ js/compiler/javac-classes.js
@@ -51,16 +51,62 @@ class JavaSourceReader extends JavacUtils {
         // Reach the class
         this.skipWhitespace(iter);
 
-        word = this.readWord(iter);
-        console.log(`word: ${word}`)
+        let classAccess = 0;
+        const accessWords = {
+            'public': Opcodes.ACC_PUBLIC,
+            'private': Opcodes.ACC_PRIVATE,
+            'protected': Opcodes.ACC_PROTECTED,
+            'static': Opcodes.ACC_STATIC,
+            'final': Opcodes.ACC_FINAL,
+            'abstract': Opcodes.ACC_ABSTRACT,
+            'deprecated': Opcodes.ACC_DEPRECATED
+            // Records aren't supported in java7
+        };
 
-        word = this.readWord(iter);
-        console.log(`word: ${word}`)
+        // Set bookmark so we can return later if needed
+        iter.setBookmark(0);
 
+        // Check class access
         word = this.readWord(iter);
-        console.log(`word: ${word}`)
+        if (word in accessWords) {
+            console.log(`classAccess: ${word}`)
+            classAccess |= accessWords[word];
 
-        // console.log(this.readPackage());
+            // Set bookmark so we can return later if needed
+            iter.setBookmark(0);
+
+            // Check class access again!
+            word = this.readWord(iter);
+            if (word in accessWords) {
+                console.log(`classAccess: ${word}`)
+                classAccess |= accessWords[word];
+            } else {
+                // Reset iter index, because we didn't find another access flag
+                iter.gotoBookmark(0);
+            }
+        } else {
+            // Reset iter index, because we didn't find any access flags
+            iter.gotoBookmark(0);
+        }
+        console.log(`accessFlags: ${classAccess}`)
+
+        // Check class-type
+        const classTypes = [
+            'class',
+            'enum',
+            'interface'
+        ];
+        const classType = this.readWord(iter);
+        console.log(`classType: ${classType}`) // classType (class, enum, interface, etc)
+        if (!classTypes.includes(classType))
+            throw new Error(`Class-type '${classType}' not supported.`);
+
+        // Read class-name
+        const className = this.readWord(iter);
+        console.log(`className: ${className}`) // className
+
+        const [ a, b, c ] = this.findOpenCloseRange(this.text, iter.index(), '{', '}');
+        console.log(a, b, c)
 
         return '';
     }
