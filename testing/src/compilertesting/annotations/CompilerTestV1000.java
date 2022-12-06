7aae2e2cb873a86d566fd33a6310269da54fcad9
HoverCatz <1442391+HoverCatz@users.noreply.github.com>
2022-12-06 07:39:53 +0100

    Update CompilerTestV1000.java

--- testing/src/compilertesting/annotations/CompilerTestV1000.java
+++ testing/src/compilertesting/annotations/CompilerTestV1000.java
@@ -1,15 +1,16 @@
-package compilertesting . annotations;
+package compilertesting.annotations;
 
 @TestAnnotation(
     abc = 123,
     def = "hello",
     ghi = {"hello", "world"},
     inner = @TestAnnotation.TestInnerAnnotation(
-        abc = 123,
-        def = "hello",
-        ghi = {"hello", "world"}
+        abc = 456,
+        def = "world",
+        ghi = {"world", "meow"}
     )
 )
+@TestAnnotation.TestInnerAnnotation(abc = 789)
 public class CompilerTestV1000 {
 
 }
