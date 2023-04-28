4e08ab2b77bdf3b3c63d1f159e37cc63ea8a6a7e
HoverCatz <1442391+HoverCatz@users.noreply.github.com>
2023-04-28 06:33:32 +0200

    parsing class header progress
    now capturing:
    - package
    - imports
    - annotations

--- js/compiler/javac-class.js
+++ js/compiler/javac-class.js
@@ -21,73 +21,238 @@ class JavaSourceReader extends JavacUtils {
     async parseSourceCode(throwErrors = true) {
         try {
             const iter = this.iter;
-            const nocc = iter.noComments(this);
-
-            // Skip leading whitespace (and all comments)
-            this.skipWhitespace(nocc);
-            if (nocc.isDone())
-                return ClassParseResult.of(ClassParseResultReason.SuccessButAbrupt, this.stripSelf());
+            const nocc = iter.noComments();
 
             const classes = [];
 
-            // Read class header
-            const header = this.readClassHeader(nocc);
-            console.log(`header:`, header)
+            // while (true) {
+            for (let i = 0; i < 100; i++) {
 
-            // Read class data
-            const data = this.readClassData(nocc);
-            console.log(`data:`, data)
+                // Skip leading whitespace (and all comments)
+                nocc.skipWhitespace();
+                if (nocc.isDone())
+                    break;
+                const start = nocc.index();
 
-            // Append resulting classes
-            this.topLevelClasses = this.topLevelClasses.concat(classes);
-
-            this.skipWhitespace(nocc);
-            if (!nocc.isDone()) {
-                // We have more classes to parse
-                const sourceReader = new JavaSourceReader(this.fileName, iter);
-                const res = await sourceReader.parseSourceCode(throwErrors);
-                if (ClassParseResult.isSuccess(res)) {
-                    const it = res.result;
-                    this.topLevelClasses = this.topLevelClasses.concat(it.topLevelClasses);
+                // Read class header
+                const header = this.readClassHeader(nocc);
+                // console.log(`header:`, header)
+                if (header === -1) {
+                    // Possible `package-info.java`, which has no curly brackets.
+                    break;
                 }
+                const [ , headerText ] = header;
+                // console.log(`headerText:`, headerText)
+
+                // Read class data
+                const data = this.readClassData(nocc);
+                // console.log(`data:`, data)
+                if (data === -1) break;
+                const [ , classText ] = data;
+
+                const parsedHeader = this.parseHeader(headerText);
+                if (!parsedHeader) break;
+                console.log(`parsedHeader:`, parsedHeader)
+
+                const parsedClass = this.parseClass(classText);
+                if (!parsedClass) break;
+                // console.log(`parsedClass:`, parsedClass)
+
+                classes.push({
+                    header: parsedHeader,
+                    data: parsedClass
+                });
             }
 
             // Check for abrupt exit
             if (classes.length === 0)
                 return ClassParseResult.of(ClassParseResultReason.SuccessButAbrupt, this.stripSelf());
 
+            // Append resulting classes
+            this.topLevelClasses = this.topLevelClasses.concat(classes);
+
             return ClassParseResult.success(this.stripSelf());
         } catch (e) {
             console.error(`Error with file: ${this.fileName}`)
             console.error(e)
+            return ClassParseResult.of(
+                ClassParseResultReason.FailedWithError,
+                {
+                    fileName: this.fileName,
+                    error: e,
+                    result: this.stripSelf()
+                }
+            );
         }
-        return ClassParseResult.of(ClassParseResultReason.FailedWithError, this.stripSelf());
     }
 
     /**
-     *
+     * Parse class statements, such as:
+     * - package <str>[.str];
+     * - import [static] <str>[.str];
+     * @param text {string}
+     * @return {false|{}}
+     */
+    parseClassStatement(text) {
+        const nocc = new CIterator(text, true);
+
+        nocc.skipWhitespace();
+        if (nocc.isDone()) return false;
+
+        const match = nocc.iterFind(
+            '^\\s*(?<left>(package|import))\\s*(?<middle>static){0,1}\\s*(?<right>([\\w$]+(\\s*\\.\\s*[\\w$]+)*(\\s*\\.\\s*\\*){0,1}))',
+            true, false);
+        if (!match) {
+            console.error(`Class statement error: Failed matching with regex: '${text}'`)
+            return false;
+        }
+
+        const left = match.groups.left;
+        const isStatic = match.groups.middle;
+        if (left === 'package' && isStatic) {
+            console.error(`Class statement error: Found 'package static': '${text}'`)
+            return false;
+        }
+        // console.log(`match:`, match)
+
+        const right = match.groups.right;
+        return {
+            [left]: right,
+            'static': !!isStatic
+        };
+    }
+
+    /**
+     * Parse the header.
+     * Extract:
+     * - package
+     * - imports
+     * - annotations
+     * - access flags
+     * - class-type
+     * - class-name
+     * - class-generics
+     * - extends (+ generics)
+     * - implements (+ generics)
+     * @param headerText {string}
+     * @returns {false|{}}
+     */
+    parseHeader(headerText) {
+        const iter = new CIterator(headerText);
+        const nocc = iter.noComments();
+        const text = nocc.text;
+
+        nocc.skipWhitespace();
+        if (nocc.isDone()) return false;
+
+        // while (true) {
+        for (let i = 0; i < 100; i++) {
+            nocc.skipWhitespace();
+            if (nocc.isDone()) break;
+
+            let start = nocc.index();
+
+            let [ index, found ] = nocc.findFirstOfMany([';', '@', '{'],
+                false, true, true, start, true);
+            if (index === -1) break;
+
+            switch (found) {
+                case ';': {
+                    const sub = text.substring(start, index);
+                    // console.log(`sub: '${sub.trim()}'`)
+                    const stmt = this.parseClassStatement(sub);
+                    console.log(`stmt:`, stmt)
+                } break;
+                case '@': {
+                    // Search for end of annotation
+                    while (true) {
+                    // for (let j = 0; j < 100; j++) {
+                        const found = nocc.iterFind('\\s*(?<first>[a-zA-Z0-9_]+)\\s*(?<second>\\S)', false, false);
+                        if (!found) break;
+                        // console.log(`found:`, found)
+
+                        const second = found.groups.second;
+                        if (second === '.') {
+                            // console.log(`middle:`, found)
+                            nocc.next(found[0].length);
+                        } else if (second === '(') {
+                            nocc.next(found[0].length - 1); // Skip (
+                            const end = nocc.index();
+                            this.skipAllBracketsBy(['('], nocc, false);
+                            const first = text.substring(index + 1, end);
+                            const last = text.substring(end, nocc.index());
+                            // console.log(`anno a: '${first}'`)
+                            // console.log(`anno b: '${last}'`)
+                            break;
+                        } else {
+                            if (second === '@' || this.isValidJavaName(second)) { // Done with annotation
+                                // console.log(`end:`, found)
+                                nocc.next(found[1].length);
+                                // console.log(`anno c: '${text.substring(index + 1, nocc.index())}'`) // Skip @
+                                break;
+                            }
+                            console.warn(`Filename: ${this.fileName}`)
+                            console.warn(`Found invalid character: '${second}' when looking for annotation data.`)
+                            break;
+                        }
+                    }
+                } break;
+                case '{':
+                    break;
+            }
+        }
+
+        return false;
+    }
+
+    parseClass(classText) {
+        return 0;
+    }
+
+    /**
+     * Detect class-header range, then parse it
      * @param nocc {CIterator}
-     * @returns {int}
+     * @returns {-1|[int,string]}
      */
     readClassHeader(nocc) {
         // Start index
         let start = nocc.index();
 
-        let idk = nocc.findFirstOfMany(['{']);
+        const [ index, ] = nocc.findFirstOfMany(['{'],
+            false, true, false, start,
+            true);
+        if (index === -1) return -1;
 
-        nocc.setIndex(9999)
-        return nocc.index();
+        const header = nocc.subString(start, index);
+        // console.log(`header:`, header)
+
+        return [ nocc.index(), header ];
     }
 
     /**
      *
      * @param nocc {CIterator}
-     * @returns {int}
+     * @returns {-1|[int,string]}
      */
     readClassData(nocc) {
+        nocc.skipWhitespace();
+        if (nocc.isDone()) return -1;
 
+        // Start index
+        let start = nocc.index();
 
-        return nocc.index();
+        const [ , found ] = nocc.findFirstOfMany(['{'],
+            false, true, false, start,
+            true);
+
+        start = nocc.index();
+        this.skipAllBracketsBy([found], nocc, false);
+        let end = nocc.index();
+
+        const data = nocc.subString(start + 1, end - 1); // We know for a fact that it
+        // console.log(`data:`, data)                  // starts and ends with a curly bracket
+
+        return [ nocc.index(), data ];
     }
 
     /**
